3ef1e5f516bed31c288ad7592b85bd9347072567
wupa9 <wupaz9@gmail.com>
2015-09-14 10:44:17 -0600

    More comments and made code a bit more flexible

--- coordinateplane.cpp
+++ coordinateplane.cpp
@@ -24,16 +24,26 @@ void CoordinatePlane::paint(QPainter *painter, const QStyleOptionGraphicsItem *o
     //Eje Y
     painter->drawLine(w/2, 0, w / 2, h);
 
+ //-----------------------------------------------------------------------------------------------------------------------------------
+// SI SE HACE UN RESIZE AL GRAPHICS VIEW, ESTAS  VARIABLES SE PUEDEN MODIFICAR
+// PARA AJUSTARLAS AL NUEVO  SIZE DEL VIEW
+
+
     //En Pixels
 
     int markerL = 10; // Longitud de cada marcador
     int interval = 25;  // Distancia en pixeles entre cada marcador
+
+    int textOffset_YAxis = 5; // Offset para acomodar el texto en el eje Y, en pixeles
+    int textOffset_XAxis = 13; // Offset para acomodar el texto en el eje X, en pixeles
 
     //En Plane Units
 
     int top = 50; // Magnitud maxima en el eje Y, en unidades de plano
     int right = 50; // Magnitud maxima en el eje X, en unidades de plano
     int _interval = 5; // Intervalo o 'skip' entre cada marcador, en unidades de plano
+
+//------------------------------------------------------------------------------------------------------------------------------------
 
     // Marcadores en el eje Y
     for(int i = 0, k = top; i <= h; i+=interval, k-=_interval)
@@ -50,7 +60,7 @@ void CoordinatePlane::paint(QPainter *painter, const QStyleOptionGraphicsItem *o
         painter->drawLine( (w/2) - markerL, i, (w/2) + markerL,  i);
 
         // Coordenada en Y
-        painter->drawText((w/2) + markerL + 5, i + 5, QString(str.c_str()));
+        painter->drawText((w/2) + markerL + textOffset_YAxis, i + textOffset_YAxis, QString(str.c_str()));
     }
 
     // Marcadores en el eje X
@@ -67,7 +77,7 @@ void CoordinatePlane::paint(QPainter *painter, const QStyleOptionGraphicsItem *o
          painter->drawLine( i, (h/2) - markerL, i,  (h/2) + markerL);
 
          //  Coordenada en X
-         painter->drawText(i,  (h/2) + markerL + 13, QString(str.c_str()));
+         painter->drawText(i,  (h/2) + markerL + textOffset_XAxis, QString(str.c_str()));
     }
 
 }
