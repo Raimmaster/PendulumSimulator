e892f9c25846f6e011020eb9a4edf6428dad4e7d
wupa9 <wupaz9@gmail.com>
2015-09-14 10:27:43 -0600

    Definir conversion de pixeles a unidades de plano y colocar varios comentarios

--- coordinateplane.cpp
+++ coordinateplane.cpp
@@ -1,6 +1,13 @@
 #include "coordinateplane.h"
 #include <QGraphicsScene>
 
+#include <sstream>
+
+#define PIXELS_TO_PLANE 5/25
+
+using std::stringstream;
+using std::string;
+
 CoordinatePlane::CoordinatePlane(float x, float y, float w, float h)
 {
     this->x = x;
@@ -11,24 +18,56 @@ CoordinatePlane::CoordinatePlane(float x, float y, float w, float h)
 
 void CoordinatePlane::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
+    //Eje X
     painter->drawLine(0, h/2, w, h/2);
+
+    //Eje Y
     painter->drawLine(w/2, 0, w / 2, h);
 
-    int markerL = 10;
-    int interval = 25;
+    //En Pixels
 
-    for(int i = 0; i <= h; i+=interval)
+    int markerL = 10; // Longitud de cada marcador
+    int interval = 25;  // Distancia en pixeles entre cada marcador
+
+    //En Plane Units
+
+    int top = 50; // Magnitud maxima en el eje Y, en unidades de plano
+    int right = 50; // Magnitud maxima en el eje X, en unidades de plano
+    int _interval = 5; // Intervalo o 'skip' entre cada marcador, en unidades de plano
+
+    // Marcadores en el eje Y
+    for(int i = 0, k = top; i <= h; i+=interval, k-=_interval)
     {
         if(i == h/2) continue;
 
+        stringstream strm("");
+        string str("");
+        strm << k;
+        strm >> str;
+
+
+        // Marcador
         painter->drawLine( (w/2) - markerL, i, (w/2) + markerL,  i);
+
+        // Coordenada en Y
+        painter->drawText((w/2) + markerL + 5, i + 5, QString(str.c_str()));
     }
 
-    for(int i =0; i <= w; i+=interval)
+    // Marcadores en el eje X
+    for(int i =0, k = -1*right; i <= w; i+=interval, k += _interval)
     {
         if(i == w/2) continue;
 
+        stringstream strm("");
+        string str("");
+        strm << k;
+        strm >> str;
+
+        // Marcador
          painter->drawLine( i, (h/2) - markerL, i,  (h/2) + markerL);
+
+         //  Coordenada en X
+         painter->drawText(i,  (h/2) + markerL + 13, QString(str.c_str()));
     }
 
 }
